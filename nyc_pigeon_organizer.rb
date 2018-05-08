0afb9700b19e1f1aabe9d242d67cafc5f93954fa
Steven Chen <ysteven.chen2@gmail.com>
2018-05-08 19:39:38 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list={}
+  data.each do |cgl,secondLevel|
+    secondLevel.each do |cgl_value,value|
+      value.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][cgl] ||= []
+        pigeon_list[name][cgl] << cgl_value.to_s
+      end
+    end
+  end
+  pigeon_list
 end
